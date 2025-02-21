eeb6cf10e03845b1209df711fccf6a09fa0480a2
MCH <111715559+mchxyz@users.noreply.github.com>
2025-02-21 12:44:59 -0500

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     console.log(props.meaning);
@@ -10,11 +11,13 @@ export default function Meaning(props) {
                 return (
                     <div key={index}>
                         <p>
-                            {definition.definition}
+                            <strong>Definition:</strong> {definition.definition}
                             <br />
+                            <strong>Example:</strong>
                             <em>{definition.example}</em>
+                            <Synonyms synonyms={definition.synonyms} />
                         </p>
-                        </div>
+                     </div>
                 );
             })}
           </div>
