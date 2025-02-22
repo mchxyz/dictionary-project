5fa7236c7f85e4696407aa79cf9a62f68b56fd63
MCH <111715559+mchxyz@users.noreply.github.com>
2025-02-22 01:13:33 -0500

    Added styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Meaning.css";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
@@ -9,13 +10,11 @@ export default function Meaning(props) {
             {
                 return (
                     <div key={index}>
-                        <p>
-                            <strong>Definition:</strong> {definition.definition}
-                            <br />
-                            <strong>Example:</strong>
-                            <em>{definition.example}</em>
+                        <div className="definition">
+                            {definition.definition}</div>
+
+                            <div className="example">{definition.example}</div>
                             <Synonyms synonyms={definition.synonyms} />
-                        </p>
                      </div>
                 );
             })}
